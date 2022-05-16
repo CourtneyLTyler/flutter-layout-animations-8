e2e18e664bc93a52f95d3c0a39892e871f7fd1a4
Courtney Tyler <courtneytyler7@gmail.com>
2022-05-16 15:26:25 -0400

    slowed down

--- lib/details.dart
+++ lib/details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'util.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 
 class Details extends StatelessWidget {
   const Details(this.creaturePath, this.creatureTag);
@@ -8,6 +9,8 @@ class Details extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //  line below slows it down (normally is 1)
+    timeDilation = 10;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
